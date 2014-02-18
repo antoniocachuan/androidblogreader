7b426d699244c05904d4e9b695a52685c181c869
Antonio Cachuan <alipazaga07@gmail.com>
2014-02-17 23:32:52 -0500

    Capitulo 2, modificado dialog para conexiones no disponibles y data no disponible

--- BlogReader/src/com/antoniocachuan/blogreader/MainListActivity.java
+++ BlogReader/src/com/antoniocachuan/blogreader/MainListActivity.java
@@ -9,23 +9,28 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	
-	protected String[] mAndroidNames;
+	protected String[] mBlogPostTitles;
 	public static final String TAG = MainListActivity.class.getSimpleName();
 	public static final int NUMBER_OF_POST = 20;
+	protected JSONObject mBlogData;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,19 +58,48 @@ public class MainListActivity extends ListActivity {
 		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 		boolean isAvailable = false;
-		if(networkInfo == null && networkInfo.isConnected()){
+		if(networkInfo != null && networkInfo.isConnected()){
 			isAvailable = true;
 		}
 			
 		return isAvailable;
 	}
 
-	private class GetBlogPostTask extends AsyncTask<Object, Void, String>{
+	private void updateList() {
+		if(mBlogData == null){ // crear un dialogo
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.error_title));//las variables error_title y error_message creadas en string.xml
+			builder.setMessage(getString(R.string.error_message));
+			builder.setPositiveButton(android.R.string.ok, null);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
+		else{
+			try {
+				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+				mBlogPostTitles = new String[jsonPosts.length()];
+				for(int i = 0; i < jsonPosts.length(); i++){
+					JSONObject post = jsonPosts.getJSONObject(i);
+					String title= post.getString("title");
+					title=Html.fromHtml(title).toString();//para convertir caracteres especiales como &htrm etc
+					mBlogPostTitles[i]=title;					
+				}
+				
+				ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+				setListAdapter(adapter);
+			} catch (JSONException e) {
+				Log.d(TAG, "Exception caught oso!");
+			}
+		}
+	}
+	
+	private class GetBlogPostTask extends AsyncTask<Object, Void, JSONObject>{
 		int responseCode = -1;
+		JSONObject jsonResponse = null;
 		@Override
-		protected String doInBackground(Object... arg0) {
+		protected JSONObject doInBackground(Object... arg0) {
 			try{
-				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=20");
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POST);
 				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
 				connection.connect();
 				
@@ -78,19 +112,10 @@ public class MainListActivity extends ListActivity {
 					reader.read(charArray);
 					String responseData = new String(charArray);
 					
-					JSONObject jsonResponse = new JSONObject(responseData);	
-					String status = jsonResponse.getString("status");
-					Log.v(TAG, status);
-					
-					JSONArray jsonPosts = jsonResponse.getJSONArray("posts");
-					
-					for (int i = 0; i < jsonPosts.length(); i++) {
-						JSONObject jsonPost = jsonPosts.getJSONObject(i);
-						String title = jsonPost.getString("title");
-						Log.v(TAG, "Post" + i + " : " + title);
-					}
+					jsonResponse = new JSONObject(responseData);	
 				}else{
 					Log.i(TAG, "Bad Http Response Code: "+ responseCode);
+					
 				}
 				
 			}
@@ -103,9 +128,18 @@ public class MainListActivity extends ListActivity {
 			catch(Exception e){
 				
 			}
-			return null;
-		}				
+			return jsonResponse;
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject result){
+			mBlogData = result;// para tenerlo en nuestra actividad traerlo del background
+			updateList();
+		}
 	}
+
+
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
